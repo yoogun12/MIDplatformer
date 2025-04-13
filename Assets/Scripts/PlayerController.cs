8a4f94b81a9a8e18d5d412853d127e57c84eee8a
yoogun12 <yoogun1313@gmail.com>
2025-04-13 18:56:24 +0900

    {Feat} 스테이지1 용암까지

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -59,5 +60,12 @@ public class PlayerController : MonoBehaviour
 
 
         transform.Translate(Vector3.right*direction*moveSpeed*Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
